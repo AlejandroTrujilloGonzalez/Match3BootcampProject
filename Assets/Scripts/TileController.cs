88dd8bf5a5d64297a1c722d28405ceac4b3cf8d9
Alejandro Trujillo <alejandro.trujillo.gon@gmail.com>
2022-07-30 17:10:36 +0200

    Art Resources + Basic UI + Basic tiles spawner

---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Tile tile;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void InitizalizeTile(Tile tile)
+    {
+        this.tile = tile;
+        spriteRenderer.sprite = TileManager.instance.tileSprites[(int) tile.type];
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("click");
+    }
+}
