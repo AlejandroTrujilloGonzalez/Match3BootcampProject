0f3751954373f6b58744bd2496ab11b803b528f2
Alejandro Trujillo <alejandro.trujillo.gon@gmail.com>
2022-09-14 21:08:40 +0200

    gitattributes test

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -12,7 +12,6 @@ public class MainMenuManager : MonoBehaviour
 
     public int maxEnergy = 5;
 
-    // Start is called before the first frame update
     void Start()
     {
         UpdateAllText();
